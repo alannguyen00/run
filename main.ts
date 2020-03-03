737e59ba20d4024201f82684f49e3a3f0e6ec954
alannguyen00 <58610714+alannguyen00@users.noreply.github.com>
2020-03-03 14:01:21 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,186 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e f e e e e e 
+f f f f f f f f e f f f f f f f 
+e e e e e e e f f f e e e e e e 
+e e f e e e e e e e e e e f e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e f e e e e e 
+e e e e e e e e e e e e e e e e 
+f f f f f f f f e e f f f f f f 
+e e e e e e e f f f f e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e f e e e e e e e e e e e e 
+e e e e e e e e e e e e e f e e 
+f f f f f f f f f f f f f f f f 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e f e e 
+`
+}
+scene.setTileMap(img`
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
+f b e e b f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f b f 
+f b e e b b b b b b b b b b b f f f f f f f f b b b b b b b b b b b b b b b b b f f f f f f f f b f 
+f b e e 7 1 1 1 1 1 1 1 1 1 b f f f f f f f f b e e e e e e e e e e e e e e e e f f f f f f f f b f 
+f b e e 7 1 1 1 1 1 1 1 1 1 b f f f f f f f f b e e e e e e e e e e e e e e e e f f f f f f f f b f 
+f b e e b b b b b b b b 1 1 b f f f f f f f f b e e b b b b b b b b b b b b b b f f f f f f f f b f 
+f b e e b f f f f f f b 1 1 b f f f f f f f f b e e b f f f f f f f f f f f f f f f f f f f f f b f 
+f b e e b f f f f f f b 1 1 b f f f f f f f f b e e b f f f f f f f f f f f f f f f f f f f f f b f 
+f b e e b f f f f f f b 1 1 b f f f f f f f f b e e b b b b b b b b b b b b f f f f f f f f f f b f 
+f b e e b f f f f f f b 1 1 b f f f f f f f f b e e e e e e e e e e e e e b f f f f f f f f f f b f 
+f b e e b f f f f f f b 1 1 b f f f f f f f f b e e e e e e e e e e e e e b f f f f f f f f f f b f 
+f b e e b f f f f f f b 1 1 b f f f f f f f f b e e b b b b b b b b b e e b f f f f f f f f f f b f 
+f b e e b f f f b b b b 1 1 b f f f f f f f f b e e b f f f f f f f b e e b f f f f f f f f f f b f 
+f b e e b f f f b 1 1 1 1 1 b f f f f f f f f b e e b f f f f f f f b e e b f f f f f f f f f f b f 
+f b e e b f f f b 1 1 1 1 1 b f f f f f f f f b e e b f f f f f f f b e e b f f f f f f f f f f b f 
+f b e e b f f f b b b b b b b f f f f f f f f b e e b f f f f f f f b e e b f f f f f f f f f f b f 
+f b e e b f f f f f f f f f f f f f f f f f f b e e b f f f f f f f b e e b f f f f f f f f f f b f 
+f b e e b f f f f f f f f f f f f f f f f f f b e e b b b b b b b b b e e b f f f f f f f f f f b f 
+f b e e b f f f f f f f f f f f f f f f f f f b e e e e e e e e e e e e e b f f f f f f f f f f b f 
+f b e e b f f f f f f f f f f f f f f f f f f b e e e e e e e e e e e e e b f f f f f f f f f f b f 
+f b e e b f f f f f f f f f f f f f f f f f f b e e b b b b b b b b b e e b f f f f f f f f f f b f 
+f b e e b f f f f f f f f f f f f f f f f f f b e e b f f f f f f f b e e b f f f f f f f f f f b f 
+f b e e b f f f f f f f f f f f f f f f f f f b e e b f f f f f f f b e e b f f f f f f f f f f b f 
+f b e e b f f f f f f f f f f f f f f f f f f b e e b f f f f f f f b e e b f f f f f f f f f f b f 
+f b e e b b b b b b b b b b b b b b b b b b b b e e b f f f f f f f b e e b f f f f f f f f f f b f 
+f b e e e e e e e e e e e e e e e e e e e e e e e e b f f f f f f f b e e b f f f f f f f f f f b f 
+f b e e e e e e e e e e e e e e e e e e e e e e e e b f f f f f f f b b b b f f f f f f f f f f b f 
+f b e e b b b b b b b e e b b b b b b b b b b b e e b f f f f f f f f f f f f f f f f f f f f f b f 
+f b e e b f f f f f b e e b f f f f f f f f f b e e b f f f f f f f f b b b b f f f f f f f f f b f 
+f b e e b f f f f f b e e b f f f f f f f f f b e e b f f f f f f f f b e e b f f f f f f f f f b f 
+f b e e b f f b b b b e e b f f f f f f f f f b e e b f f f f f f f f b e e b f f f f f f f f f b f 
+f b e e b f f b e e e e e b f f f f f f f f f b e e b f f f f f f f f b e e b f f f f f f f f f b f 
+f b e e b f f b e e e e e b f f f f f f f f f b e e b f f f f f f f f b e e b f f f f f f f f f b f 
+f b e e b f f b b b b e e b b b b b b b b b b b e e b b b b b b b b b b e e b f f f f f f f f f b f 
+f b e e b f f f f f b e e e e e e e e e e e e e e e e e e e e e e e e e e e b f f f f f f f f f b f 
+f b e e b f f f b b b e e e e e e e e e e e e e e e e e e e e e e e e e e e b e e b b b b b b b b f 
+f b e e b f f f b b b e e b b b b b b b b b b b e e b b b b b b b b b b e e b e e e e e e e e b b f 
+f b e e b f f f b b b e e b b b b b b b b b b b e e b e e e e e e e e e e e b e e e e e e e e b b f 
+f b e e b f f f b b b e e b b b b b b b b b b b e e b e e e e e e e e e e e b e e b b b b b b b b f 
+f b e e b f f f f b b e e b b b b b b b b b b b e e b e e e e e e e e e e e b e e b f f f f f f b f 
+f b e e b b b b b b b e e b b b b b b b b b b b e e b b b b b b b b b b b b b e e b b b b b b b b f 
+f b e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e b b f 
+f b e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e b b f 
+f b b b b b b b b b b e e b b b b b b b b b b b e e b b b b b b b b b b b b b b b b b b b b b b b f 
+f f f f f f f f f f b e e b f f f f f f f f f b e e b f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f b e e b f f f f f f f f f b e e b f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f b e e b f f f f f f f f f b e e b f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f b e e b f f f f f f f f f b e e b f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f b b b b f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+`)
+let Carlos = sprites.create(img`
+. . . . . . 1 1 1 1 1 1 1 . . . . 
+. . . . . . 1 f f 1 f f 1 . . . . 
+. . . . . . 1 f f 1 f f 1 . . . . 
+. . . . . . 1 f f 1 f f 1 . . . . 
+. . . . . . 1 f f 1 f f 1 . 1 1 . 
+. . . . . . 1 1 1 1 1 1 1 . f 1 1 
+. . . . . . 2 2 2 2 2 2 1 . f 1 1 
+. . . . . 2 e 2 2 2 2 e 2 . f 1 1 
+. . . . 2 2 2 e 2 2 e 2 2 d d . . 
+. . . d d 2 2 2 e e 2 2 d d f . . 
+. . d d 2 2 2 2 e e 2 2 2 . . . . 
+. . d . . 2 2 2 e e 2 2 2 . . . . 
+. . . . . . 8 8 8 8 8 8 . . . . . 
+. . . . . . 8 8 . . 8 8 . . . . . 
+. . . . . . 8 8 . . 8 8 . . . . . 
+. . . . . . 8 8 . . 8 8 . . . . . 
+. . . . . f f f . . f f f . . . . 
+`, SpriteKind.Player)
+controller.moveSprite(Carlos, 150, 150)
+scene.cameraFollowSprite(Carlos)
+scene.setTile(11, img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+`, true)
+scene.setTile(7, img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+`, false)
+scene.setTile(14, img`
+e e f e e e e e e e e e e e e e 
+e e e e e e e e e f e e e e e e 
+e e e e e e e e e e e e e e e e 
+f f f f f f f f e f f f f f f f 
+e e e f e e e f f f e e e e e e 
+e e e e e e e e e e e e e f e e 
+e e e f e e e e e e e e e e e e 
+e e e e e e e e e e f e e e e e 
+e e f f f f e e e e e e e e e e 
+f f f e e f f f f f f f f f f f 
+e e e e e e e e e e e e e f e e 
+e f e e e e e e e e e e e e e e 
+e e e e e e e f e e e e e e e e 
+f f f f f f f f f f f f f f f f 
+e e e e e e e e e e e e e e f f 
+e e e e e f e e e e e e e e e e 
+`, false)
+scene.setTile(1, img`
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e f f f f f f e e e f f e e e e 
+f f e e e e f f f f f e f f f f 
+f e e e e e e e e e 3 3 e e e e 
+e e e e 3 e e e e e e 3 3 3 e e 
+e e e e 3 3 e e e e e e e 3 3 e 
+f e e e e 3 3 e e e e e e e e e 
+f f f f f f 3 f f f f f f f f f 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e 3 e e e e e e 3 f f f e e e 
+f f 3 f f f f 3 3 3 f e f f f f 
+e e 3 e e e e e e e e e e e e e 
+e e 3 e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`, false)
+Carlos.setPosition(400, 775)
